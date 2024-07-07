fcf6f3ce760232288cc048fef50746e809ad9451
Luis Santamaria <luiisant@espol.edu.ec>
2024-07-07 12:36:57 -0500

    Primer commit

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["C:\Users\santa\OneDrive\Escritorio\Luis\Mi pagina cv\principal.{html,css}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
