4fb3e97d78845e5133202a9e9e8ed074492de91a
Luis Santamaria <luiisant@espol.edu.ec>
2025-03-12 16:28:15 -0500

    Creacion de pagina SEL + CV ingles

---
+++ src/sel.js
@@ -0,0 +1,3 @@
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("video-SEL").innerHTML = `<iframe width="560" height="315" src="https://youtu.be/Al_WbyKFpdI" frameborder="0" allowfullscreen></iframe>`;
+});
